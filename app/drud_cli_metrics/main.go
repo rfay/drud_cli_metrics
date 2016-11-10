e76457e882f674783286131d1b8f8e995725bf37
Randy Fay <randy@randyfay.com>
2016-11-10 08:22:40 -0700

    Simplifying write and marshalling

--- app/drud_cli_metrics/main.go
+++ app/drud_cli_metrics/main.go
@@ -2,6 +2,7 @@ package main
 
 // Microservice to accept logging from drud cli tool
 // Many thanks for
+//   database/sql tutorial: http://go-database-sql.org/
 //   sqlite3: https://siongui.github.io/2016/01/09/go-sqlite-example-basic-usage/
 //   sqlite3: https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/05.3.html
 //   json API: https://www.thepolyglotdeveloper.com/2016/07/create-a-simple-restful-api-with-golang/
@@ -35,10 +36,10 @@ var pDb *sql.DB
 var pDbFilepath = flag.String("path", "/var/lib/sqlite3/drud_cli_metrics.db", "Full path to the sqlite3 database file which will be created if it does not exist.")
 var pServerPort = flag.Int("port", 12345, "Port on which service should listen")
 
-func GetLogEndpoint(w http.ResponseWriter, req *http.Request) {
+func getLogEndpoint(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	id, _ := strconv.Atoi(params["id"])
-	item, err := ReadItem(pDb, id)
+	item, err := readItem(pDb, id)
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 	} else {
@@ -46,23 +47,30 @@ func GetLogEndpoint(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func GetAllLogsEndpoint(w http.ResponseWriter, req *http.Request) {
-	json.NewEncoder(w).Encode(ReadAllItems(pDb))
+func getAllLogsEndpoint(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(readAllItems(pDb))
 }
 
-func CreateLogEndpoint(w http.ResponseWriter, req *http.Request) {
-	var person LogItem
-	result := json.NewDecoder(req.Body).Decode(&person)
-	if result != nil {
-		fmt.Fprintf(os.Stderr, "result=%v\n", result)
+func createLogEndpoint(w http.ResponseWriter, req *http.Request) {
+	var logItem LogItem
+	log.Printf("createLogEndpoint: Incoming body=%v", req.Body)
+
+	err := json.NewDecoder(req.Body).Decode(&logItem)
+	if err != nil {
+		log.Printf("json.NewDecoder() error result=%v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	log.Printf("createLogEndpoint: Incoming logItem=%v", logItem)
 
-	StoreItem(pDb, person)
-	json.NewEncoder(w).Encode(ReadAllItems(pDb))
+	count, _ := storeItem(pDb, logItem)
+	if count != 1 {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	json.NewEncoder(w).Encode(readAllItems(pDb))
 }
 
-func UpdateLogEndpoint(w http.ResponseWriter, req *http.Request) {
+func updateLogEndpoint(w http.ResponseWriter, req *http.Request) {
 	var log LogItem
 	params := mux.Vars(req)
 	id, _ := strconv.Atoi(params["id"])
@@ -70,27 +78,24 @@ func UpdateLogEndpoint(w http.ResponseWriter, req *http.Request) {
 	checkErr(err)
 	log.ID = id
 
-	StoreItem(pDb, log)
+	storeItem(pDb, log)
 
-	json.NewEncoder(w).Encode(ReadAllItems(pDb))
+	json.NewEncoder(w).Encode(readAllItems(pDb))
 }
 
-func DeleteLogEndpoint(w http.ResponseWriter, req *http.Request) {
+func deleteLogEndpoint(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	id, _ := strconv.Atoi(params["id"])
 	fmt.Fprintf(os.Stderr, "Deleting id=%d\n", id)
-	DeleteItem(pDb, id)
-	json.NewEncoder(w).Encode(ReadAllItems(pDb))
+	deleteItem(pDb, id)
+	json.NewEncoder(w).Encode(readAllItems(pDb))
 }
 
-func LivenessEndpoint(w http.ResponseWriter, req *http.Request) {
+func livenessEndpoint(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode("alive")
 }
-func ReadinessEndpoint(w http.ResponseWriter, req *http.Request) {
-	json.NewEncoder(w).Encode("ready")
-}
 
-func InitDB(filepath string) *sql.DB {
+func initDB(filepath string) *sql.DB {
 	if _, err := os.Stat(filepath); os.IsNotExist(err) {
 		log.Printf("DB File %s does not exist so will be created", filepath)
 	} else {
@@ -107,7 +112,7 @@ func InitDB(filepath string) *sql.DB {
 	return db
 }
 
-func CreateTable(db *sql.DB) {
+func createLogsTable(db *sql.DB) {
 	// create table if not exists
 	sql_table := `
 	create table if not exists logs (
@@ -126,58 +131,52 @@ func CreateTable(db *sql.DB) {
 	}
 }
 
-func StoreItem(db *sql.DB, item LogItem) {
-	var items []LogItem
-	items = append(items, item)
-	StoreItems(db, items)
-}
-
-func StoreItems(db *sql.DB, items []LogItem) {
-	sqlAddItemWithIdQuery := `
-	INSERT OR REPLACE INTO logs (
+func storeItem(db *sql.DB, item LogItem) (int64, error) {
+	log.Printf("storeItem: Incoming item=%v", item)
+	var res sql.Result
+	if item.ID != 0 {
+		upsertQuery := `INSERT OR REPLACE INTO logs (
 		ID,
 		clientTimestamp,
 		resultCode,
 		machineId,
 		info,
 		InsertedDatetime
-	) values(?, ?, ?, ?, ?, CURRENT_TIMESTAMP)
-	`
-	sqlAddItemNoIdQuery := `
-	INSERT INTO logs (
+	        ) values(?, ?, ?, ?, ?, CURRENT_TIMESTAMP)`
+
+		stmt, err := db.Prepare(upsertQuery)
+		if err != nil {
+			log.Fatal(err)
+		}
+		res, err = stmt.Exec(item.ID, item.clientTimestamp, item.resultCode, item.machineId, item.info)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		insertQuery := `insert into logs (
 		clientTimestamp,
 		resultCode,
 		machineId,
 		info,
 		InsertedDatetime
-	) values(?, ?, ?, ?, CURRENT_TIMESTAMP)`
+	        ) values(?, ?, ?, ?, CURRENT_TIMESTAMP)`
 
-	idStatement, err := db.Prepare(sqlAddItemWithIdQuery)
-	if err != nil {
-		panic(err)
-	}
-	defer idStatement.Close()
-
-	noIdStatement, err := db.Prepare(sqlAddItemNoIdQuery)
-	if err != nil {
-		panic(err)
-	}
-	defer noIdStatement.Close()
-
-	for _, item := range items {
-		// If ID is provided, do upsert, otherwise do insert
-		if item.ID != 0 {
-			_, err := idStatement.Exec(item.ID, item.clientTimestamp, item.resultCode, item.machineId, item.info)
-			checkErr(err)
-		} else {
-			_, err := noIdStatement.Exec(item.clientTimestamp, item.resultCode, item.machineId, item.info)
-			checkErr(err)
+		stmt, err := db.Prepare(insertQuery)
+		if err != nil {
+			log.Fatal(err)
+		}
+		res, err = stmt.Exec(item.clientTimestamp, item.resultCode, item.machineId, item.info)
+		if err != nil {
+			log.Fatal(err)
 		}
 
 	}
+
+	return res.RowsAffected()
+
 }
 
-func ReadAllItems(db *sql.DB) []LogItem {
+func readAllItems(db *sql.DB) []LogItem {
 	sql_readall := `
 	select * from logs
 	order by datetime(InsertedDatetime)
@@ -201,7 +200,7 @@ func ReadAllItems(db *sql.DB) []LogItem {
 	return result
 }
 
-func ReadItem(db *sql.DB, id int) (LogItem, error) {
+func readItem(db *sql.DB, id int) (LogItem, error) {
 	sqlReadOne := `
 	select * from logs
 	where ID = ?
@@ -212,7 +211,7 @@ func ReadItem(db *sql.DB, id int) (LogItem, error) {
 	return item, err
 }
 
-func DeleteItem(db *sql.DB, id int) {
+func deleteItem(db *sql.DB, id int) {
 	sql_delete := `
 	delete from logs
 	where ID = ?
@@ -238,24 +237,26 @@ func main() {
 	log.Printf("Will listen on port=%d\n", *pServerPort)
 	log.Printf("Sqlite DB file fullpath=%s\n", *pDbFilepath)
 
-	pDb = InitDB(*pDbFilepath)
-	CreateTable(pDb)
+	pDb = initDB(*pDbFilepath)
+	defer pDb.Close()
+
+	createLogsTable(pDb)
 
 	router := mux.NewRouter()
 	// Read all command log and return
-	router.HandleFunc("/v1.0/log", GetAllLogsEndpoint).Methods("GET")
+	router.HandleFunc("/v1.0/log", getAllLogsEndpoint).Methods("GET")
 	// Create a command log - ID is automatically incremented
-	router.HandleFunc("/v1.0/log", CreateLogEndpoint).Methods("POST")
-	// Get a single person
-	router.HandleFunc("/v1.0/log/{id}", GetLogEndpoint).Methods("GET")
-	// Update a single person
-	router.HandleFunc("/v1.0/log/{id}", UpdateLogEndpoint).Methods("POST")
+	router.HandleFunc("/v1.0/log", createLogEndpoint).Methods("POST")
+	// Get a single log
+	router.HandleFunc("/v1.0/log/{id}", getLogEndpoint).Methods("GET")
+	// Update a single log
+	router.HandleFunc("/v1.0/log/{id}", updateLogEndpoint).Methods("POST")
 	// Delete an item by id
-	router.HandleFunc("/v1.0/log/{id}", DeleteLogEndpoint).Methods("DELETE")
+	router.HandleFunc("/v1.0/log/{id}", deleteLogEndpoint).Methods("DELETE")
 	// Readiness probe
-	router.HandleFunc("/readiness", ReadinessEndpoint).Methods("GET")
+	router.HandleFunc("/readiness", livenessEndpoint).Methods("GET")
 	// Liveness probe
-	router.HandleFunc("/healthz", LivenessEndpoint).Methods("GET")
+	router.HandleFunc("/healthz", livenessEndpoint).Methods("GET")
 
 	// Listen on port
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *pServerPort), router))
